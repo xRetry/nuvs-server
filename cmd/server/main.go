525330c1570da820fe912a958d530c2119be6828
xRetry <marcoschwarz.at@gmail.com>
2022-12-04 17:45:10 +0100

    Fix json formatting

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -4,7 +4,6 @@ import (
 	"sync"
 	"github.com/xRetry/nuvs-server/internal/udp-service"
 	"github.com/xRetry/nuvs-server/internal/response"
-	"time"
 )
 
 var waitGroup sync.WaitGroup
@@ -14,11 +13,9 @@ func main() {
 	ch := make(chan map[string]udp_service.Record, 1)
 
 	m := make(map[string]udp_service.Record) 
-	m["sfds"] = udp_service.Record{"23.42", "dfsfsfd", time.Now()}
-	m["dfse"] = udp_service.Record{"23.45.2", "dfsfsfd", time.Now()}
 	ch <- m
 
-	waitGroup.Add(3)
+	waitGroup.Add(2)
 
 	go udp_service.RunUdpService(ch)
 	go response.RunResponseRoutine(ch)
